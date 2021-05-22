b4eeb8ff39c38d3932d21baa528e07fe901f2aec
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-05-22 23:16:55 +0300

    [render_coin.c] fix render_coin

--- src/render_coin.c
+++ src/render_coin.c
@@ -138,8 +138,8 @@ void	render_coin(t_wolf *wolf, SDL_Surface *surface)
 		* (wolf->bon->image_coin->w + 1) / ((W / 32) \
 		* (wolf->player->dist_to_canvas / coin.dist)));
 	coin.cut_vertical_img.h = (wolf->bon->image_coin->w + 1);
-	coin.cut_vertical_img.x = (int)(coin.flag_1 * (wolf->bon->image_coin->w + 1) \
-	/ ((W / 32) * (wolf->player->dist_to_canvas / coin.dist)));
+	coin.cut_vertical_img.x = (int)(coin.flag_1 * (wolf->bon->image_coin->w
+				+ 1) / ((W / 32) * (wolf->player->dist_to_canvas / coin.dist)));
 	coin.cut_vertical_img.y = 0;
 	coin.img_location.w = (coin.flag_2 - coin.flag_1);
 	coin.img_location.h = (W / 32) * (wolf->player->dist_to_canvas / coin.dist);
