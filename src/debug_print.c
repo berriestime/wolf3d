5dd088514225b4fbfc333072e339adfaac3568bd
Rustam @SecondFry Gubaydullin <secondfry+github@gmail.com>
2021-05-26 23:28:10 +0300

    [debug_print.c] Change to return-early

--- src/debug_print.c
+++ src/debug_print.c
@@ -14,32 +14,30 @@
 
 void	debug_map(t_map *map)
 {
-	if (DEBUG)
-	{
-		ft_putstr(C_YEL);
-		ft_putstr(" MAP INFO ");
-		ft_putendl(C_NRM);
-		ft_putnbr(map->w);
-		ft_putstr(" map->w | ");
-		ft_putnbr(map->h);
-		ft_putendl(" map->h");
-	}
+	if (!DEBUG)
+		return ;
+	ft_putstr(C_YEL);
+	ft_putstr(" MAP INFO ");
+	ft_putendl(C_NRM);
+	ft_putnbr(map->w);
+	ft_putstr(" map->w | ");
+	ft_putnbr(map->h);
+	ft_putendl(" map->h");
 }
 
 void	debug_player(t_player *p)
 {
-	if (DEBUG)
-	{
-		ft_putstr(C_GRN);
-		ft_putstr(" PLAYER INFO ");
-		ft_putendl(C_NRM);
-		ft_putnbr(p->x * 1000);
-		ft_putstr(" p->x | ");
-		ft_putnbr(p->y * 1000);
-		ft_putendl(" p->y");
-		ft_putnbr(p->dir * 1000);
-		ft_putstr(" p->dir | ");
-		ft_putnbr(p->fov * 1000);
-		ft_putendl(" p->fov");
-	}
+	if (!DEBUG)
+		return ;
+	ft_putstr(C_GRN);
+	ft_putstr(" PLAYER INFO ");
+	ft_putendl(C_NRM);
+	ft_putnbr(p->x * 1000);
+	ft_putstr(" p->x | ");
+	ft_putnbr(p->y * 1000);
+	ft_putendl(" p->y");
+	ft_putnbr(p->dir * 1000);
+	ft_putstr(" p->dir | ");
+	ft_putnbr(p->fov * 1000);
+	ft_putendl(" p->fov");
 }
